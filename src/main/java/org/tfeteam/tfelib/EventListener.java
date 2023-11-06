5b8b9f346deda5fad40b2b8fe95cbf408e9e5eba
realSeppo <SpbMsc25@mail.ru>
2023-11-06 18:00:24 +0300

    7

--- src/main/java/org/tfeteam/tfelib/EventListener.java
+++ src/main/java/org/tfeteam/tfelib/EventListener.java
@@ -1,6 +1,7 @@
 package org.tfeteam.tfelib;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
@@ -8,12 +9,14 @@ import org.bukkit.inventory.InventoryHolder;
 import org.tfeteam.tfelib.Menu.Menu;
 
 public class EventListener implements Listener {
+    @EventHandler
     public void onItemClick(InventoryClickEvent event) {
         InventoryHolder inventoryHolder = event.getInventory().getHolder();
         if (inventoryHolder instanceof Menu) {
             ((Menu) inventoryHolder).interact(event.getSlot(), (Player) event.getWhoClicked());
         }
     }
+    @EventHandler
     public void onItemDrag(InventoryDragEvent event) {
         InventoryHolder inventoryHolder = event.getInventory().getHolder();
         if (inventoryHolder instanceof Menu) {
