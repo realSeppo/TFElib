363ef2feff702616321cbc0cf3f020e54060d5fd
realSeppo <SpbMsc25@mail.ru>
2023-12-03 13:36:01 +0300

    itemsaddermenus+

--- src/main/java/org/tfeteam/tfelib/Menu/Menu.java
+++ src/main/java/org/tfeteam/tfelib/Menu/Menu.java
@@ -2,6 +2,7 @@ package org.tfeteam.tfelib.Menu;
 
 import dev.lone.itemsadder.api.FontImages.FontImageWrapper;
 import dev.lone.itemsadder.api.FontImages.TexturedInventoryWrapper;
+import dev.lone.itemsadder.api.ItemsAdder;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
@@ -14,12 +15,14 @@ import org.jetbrains.annotations.NotNull;
 
 public abstract class Menu implements InventoryHolder {
     Inventory inventory;
+    TexturedInventoryWrapper texturedInventory;
 
     public Menu(int slots, String name){
         inventory = Bukkit.createInventory(this, slots, name);
     }
     public Menu(int slots, String name, String texture, int titleOffset, int textureOffset){
-        inventory = new TexturedInventoryWrapper(this, 27, name, titleOffset, textureOffset, new FontImageWrapper(texture)).getInternal();
+        texturedInventory = new TexturedInventoryWrapper(this, 27, name, titleOffset, textureOffset, new FontImageWrapper(texture));
+        inventory = texturedInventory.getInternal();
     }
 
     protected void fill(ItemStack item){
@@ -36,6 +39,11 @@ public abstract class Menu implements InventoryHolder {
     public ItemStack getItem(int slot) {return inventory.getItem(slot);}
 
     public abstract boolean interact(int slot, Player player);
+
+    public void openInventory(Player player){
+        if(texturedInventory != null) texturedInventory.showInventory(player);
+        else player.openInventory(inventory);
+    }
 
     @Override
     public @NotNull Inventory getInventory() {
